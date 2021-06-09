ae8683e0b222e27427af50b4bed7b2fa010570c4
SyedMehdiRaza <mehdiraza1232016@gmail.com>
2021-06-09 11:48:40 -0700

    simple js calculator

---
+++ app.js
@@ -0,0 +1,16 @@
+function getnumber(num) {
+var screen = document.getElementById("screen");
+screen.value += num;
+}
+
+function clearnum() {
+var screen = document.getElementById('screen');
+screen.value = "";
+}
+
+function result() {
+    var screen = document.getElementById("screen");
+    screen.value = eval(screen.value);
+}
+
+
